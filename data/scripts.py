2868129f7fe8362f1aeb33278b52361d82a2eb03
RodrigoJara25 <r.jara.e7@gmail.com>
2024-11-29 10:54:07 -0500

    inserts v2

--- data/scripts.py
+++ data/scripts.py
@@ -115,10 +115,61 @@ def llenar_postulantes():
     with open('inserts_postulantes.sql', 'w', encoding = 'utf-8') as file:
         file.write(contenido)
 
+def llenar_proyectos():
+    contenido = ''
+    i = 0
+    meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+    years = [2020, 2021, 2022, 2023, 2024]
+    while i < 200:
+        id = i + 1
+        titulo = fake.sentence()
+        descripcion = fake.paragraph()
+        n_random = random.randint(0, 11)
+        mes_inicio = meses[n_random]
+        n_random = random.randint(0, 11)
+        mes_final = meses[n_random]
+        n_random = random.randint(0, 4)
+        anio_inicio = years[n_random]
+        n_random = random.randint(0, 4)
+        anio_final = years[n_random]
+        postulante_id = random.randint(1, 200)
+        tipos_proyecto_id = random.randint(1, 3)
+        tmp = f"INSERT INTO proyectos (id, titulo, descripcion, mes_inicio, mes_final, anio_inicio, anio_final, postulante_id, tipos_proyecto_id) VALUES ({id}, '{titulo}', '{descripcion}', '{mes_inicio}', '{mes_final}', {anio_inicio}, {anio_final}, {postulante_id}, {tipos_proyecto_id});\n"
+        contenido = contenido + tmp
+        i = i + 1
+    
+    with open('inserts_proyectos.sql', 'w', encoding = 'utf-8') as file:
+        file.write(contenido)
+
+def llenar_empresas():
+    contenido = ''
+    i = 1
+    with open('nombres_empresas.txt', mode='r', encoding='utf-8') as archivo:
+        # Itera sobre las filas (son 50 empresas)
+        for fila in archivo:
+            id = i 
+            nombre = fila.strip()
+            cantidad_empleados = random.randint(1, 10)
+            contacto = fake.url()
+            informacion_general = fake.paragraph()
+            industria_id = random.randint(1, 20)
+            tipo_empresa_id = random.randint(1, 4)
+            tmp = f"INSERT INTO empresas (id, nombre, cantidad_empleados, contacto, informacion_general, industria_id, tipo_empresa_id) VALUES ({id}, '{nombre}', {cantidad_empleados}, '{contacto}', '{informacion_general}', {industria_id}, {tipo_empresa_id});\n"
+            contenido = contenido + tmp
+            i = i + 1
+
+    with open('inserts_empresas.sql', 'w', encoding = 'utf-8') as file:
+        file.write(contenido)
+
+
+
 # llenar_producto()
 # llenar_clientes()
 # llenar_facturas()
 
 # llenar_experiencias()
 # llenar_codigo_postal()
-llenar_postulantes()
+# llenar_postulantes()
+# llenar_proyectos()
+llenar_empresas()
